0c4c5dc656f79ff309dadd677e1bf7138d1e4c38
praz <t.prazwal@gmail.com>
2021-01-19 20:28:41 +0100

    move reducer and actions to different files

--- src/App.js
+++ src/App.js
@@ -4,52 +4,30 @@ import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 // items
 import cartItems from "./cart-items";
+
 // redux stuff
-
 import { createStore } from 'redux';
-// initial store
 
+import { DECREASE, INCREASE } from './actions';
+
+import reducer from './reducer';
+
+// initial store
 const initialStore = {
-  count: 0,
-  name: 'john',
+  cart: cartItems,
+  total: 0,
+  amount: 0,
 };
 
-// reducer
-
-function reducer(state, action) {
-  console.log({ state, action });
-  if(action.type === 'DECREASE') {
-    return { ...state, count: state.count - 1, name: 'anna' }
-  }
-
-  if(action.type === 'INCREASE') {
-    return { ...state, count: state.count + 1 }
-  }
-
-  if(action.type === 'RESET') {
-    return { ...state, count: 0 }
-  }
-
-  if(action.type === 'CHANGE_NAME') {
-    return { ...state, name: 'bob' }
-  }
-
-  return state;
-}
 
 const store = createStore(reducer, initialStore);
-store.dispatch({ type: 'DECREASE' })
-store.dispatch({ type: 'CHANGE_NAME' })
-store.dispatch({ type: 'INCREASE' })
-store.dispatch({ type: 'RESET' })
-console.log(store.getState());
 
 function App() {
   // cart setup
 
   return (
     <main>
-      <Navbar cart={store.getState()} />
+      <Navbar />
       <CartContainer cart={cartItems} />
     </main>
   );
