c7a9d1cfca85efe1fb3b4b0869a94b7269337909
praz <t.prazwal@gmail.com>
2021-01-19 20:16:21 +0100

    add reducer function and dispatch

--- src/App.js
+++ src/App.js
@@ -6,12 +6,50 @@ import CartContainer from "./components/CartContainer";
 import cartItems from "./cart-items";
 // redux stuff
 
+import { createStore } from 'redux';
+// initial store
+
+const initialStore = {
+  count: 0,
+  name: 'john',
+};
+
+// reducer
+
+function reducer(state, action) {
+  console.log({ state, action });
+  if(action.type === 'DECREASE') {
+    return { ...state, count: state.count - 1, name: 'anna' }
+  }
+
+  if(action.type === 'INCREASE') {
+    return { ...state, count: state.count + 1 }
+  }
+
+  if(action.type === 'RESET') {
+    return { ...state, count: 0 }
+  }
+
+  if(action.type === 'CHANGE_NAME') {
+    return { ...state, name: 'bob' }
+  }
+
+  return state;
+}
+
+const store = createStore(reducer, initialStore);
+store.dispatch({ type: 'DECREASE' })
+store.dispatch({ type: 'CHANGE_NAME' })
+store.dispatch({ type: 'INCREASE' })
+store.dispatch({ type: 'RESET' })
+console.log(store.getState());
+
 function App() {
   // cart setup
 
   return (
     <main>
-      <Navbar />
+      <Navbar cart={store.getState()} />
       <CartContainer cart={cartItems} />
     </main>
   );
