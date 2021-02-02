020411c69092bb248b30be5e183e18572f0e4731
praz <t.prazwal@gmail.com>
2021-02-02 20:14:47 +0100

    add functionality to remove items from cart

--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,21 @@
-import { DECREASE, INCREASE, CLEAR_CART } from './actions';
+import { DECREASE, INCREASE, REMOVE, CLEAR_CART } from './actions';
 
 function reducer(state, action) {
   if (action.type === CLEAR_CART) {
     return { ...state, cart: [] };
-  } 
+  }
+
+  if(action.type === DECREASE) {
+    console.log("decreased");
+  }
+
+  if(action.type === INCREASE) {
+    console.log("increased");
+  }
+
+  if(action.type === REMOVE) {
+    return {...state, cart: state.cart.filter(cartItem => cartItem.id !== action.payload.id)}
+  }
   return state;
 }
 
