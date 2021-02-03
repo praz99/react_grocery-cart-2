a88158c21aef0e0d1943e46ce9ae1dac5205806c
praz <t.prazwal@gmail.com>
2021-02-03 13:28:44 +0100

    calculate and display total price.

--- src/reducer.js
+++ src/reducer.js
@@ -37,6 +37,9 @@ function reducer(state, action) {
   if(action.type === GET_TOTALS) {
     let { total, amount } = state.cart.reduce((cartTotal, cartItem) => {
       const { price, amount } = cartItem;
+      const itemTotal = price * amount;
+
+      cartTotal.total += itemTotal;
       cartTotal.amount += amount
       return cartTotal;
     },
@@ -44,6 +47,7 @@ function reducer(state, action) {
       total: 0,
       amount: 0,
     });
+    total = parseFloat(total.toFixed(2));
     return { ...state, total, amount };
   }
   return state;
